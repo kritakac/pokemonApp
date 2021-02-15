50541de47637678d22c257fd1db6e1b709b2b369
Kristina <kritakac23@gmail.com>
2021-02-15 10:01:01 +0100

    initial commit

---
+++ src/test/java/com/kristina/pokemonapp/PokemonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristina.pokemonapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
